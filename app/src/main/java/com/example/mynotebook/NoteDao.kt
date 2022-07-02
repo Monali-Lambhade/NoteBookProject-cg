b1daa8dea650a941a0a5e813d993afc590b50c33
Monali-Lambhade <80144465+Monali-Lambhade@users.noreply.github.com>
2022-07-02 17:53:36 +0530

    Notebook Application

---
+++ app/src/main/java/com/example/mynotebook/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.mynotebook
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes():LiveData<List<Note>>
+    //suspend fun getAllNotes1(): List<Note>
+}
+
+
